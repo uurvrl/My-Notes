ccefa4c4f4f7f6d6f65e1bb1a5b6a080b7310f5e
uurvrl <107815698+uurvrl@users.noreply.github.com>
2022-08-04 04:00:57 +0300

    Add files via upload

---
+++ Miuul-Notes/Miuul_Functions.py
@@ -0,0 +1,413 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
+import warnings
+warnings.simplefilter(action="ignore")
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+pd.set_option('display.float_format', lambda x: '%.3f' % x)
+pd.set_option('display.width', 500)
+
+
+def check_df(dataframe, head=5):
+    print("##################### Shape #####################")
+    print(dataframe.shape)
+    print("##################### Types #####################")
+    print(dataframe.dtypes)
+    print("##################### Head #####################")
+    print(dataframe.head(head))
+    print("##################### Tail #####################")
+    print(dataframe.tail(head))
+    print("##################### NA #####################")
+    print(dataframe.isnull().sum())
+    print("##################### Quantiles #####################")
+    print(dataframe.quantile([0, 0.05, 0.50, 0.95, 0.99, 1]).T)
+
+def target_summary_with_cat(dataframe, target, categorical_col):
+    print(categorical_col)
+    print(pd.DataFrame({"TARGET_MEAN": dataframe.groupby(categorical_col)[target].mean(),
+                        "Count": dataframe[categorical_col].value_counts(),
+                        "Ratio": 100 * dataframe[categorical_col].value_counts() / len(dataframe)}), end="\n\n\n")
+
+def target_summary_with_num(dataframe, target, numerical_col):
+    print(dataframe.groupby(target).agg({numerical_col: "mean"}), end="\n\n\n")
+
+def outlier_thresholds(dataframe, col_name, q1=0.25, q3=0.75):
+    quartile1 = dataframe[col_name].quantile(q1)
+    quartile3 = dataframe[col_name].quantile(q3)
+    interquantile_range = quartile3 - quartile1
+    up_limit = quartile3 + 1.5 * interquantile_range
+    low_limit = quartile1 - 1.5 * interquantile_range
+    return low_limit, up_limit
+
+def check_outlier(dataframe, col_name, q1=.25, q3=.75):
+    low_limit, up_limit = outlier_thresholds(dataframe, col_name, q1, q3)
+    if dataframe[(dataframe[col_name] > up_limit) | (dataframe[col_name] < low_limit)].any(axis=None):
+        return True
+    else:
+        return False
+
+def grab_col_names(dataframe, cat_th=10, car_th=20):
+    """
+
+    Veri setindeki kategorik, numerik ve kategorik fakat kardinal değişkenlerin isimlerini verir.
+    Not: Kategorik değişkenlerin içerisine numerik görünümlü kategorik değişkenler de dahildir.
+
+    Parameters
+    ------
+        dataframe: dataframe
+                Değişken isimleri alınmak istenilen dataframe
+        cat_th: int, optional
+                numerik fakat kategorik olan değişkenler için sınıf eşik değeri
+        car_th: int, optinal
+                kategorik fakat kardinal değişkenler için sınıf eşik değeri
+
+    Returns
+    ------
+        cat_cols: list
+                Kategorik değişken listesi
+        num_cols: list
+                Numerik değişken listesi
+        cat_but_car: list
+                Kategorik görünümlü kardinal değişken listesi
+
+    Examples
+    ------
+        import seaborn as sns
+        df = sns.load_dataset("iris")
+        print(grab_col_names(df))
+
+
+    Notes
+    ------
+        cat_cols + num_cols + cat_but_car = toplam değişken sayısı
+        num_but_cat cat_cols'un içerisinde.
+        Return olan 3 liste toplamı toplam değişken sayısına eşittir: cat_cols + num_cols + cat_but_car = değişken sayısı
+
+    """
+
+    # cat_cols, cat_but_car
+    cat_cols = [col for col in dataframe.columns if dataframe[col].dtype == "O"]
+    num_but_cat = [col for col in dataframe.columns if dataframe[col].nunique() < cat_th and
+                   dataframe[col].dtype != "O"]
+    cat_but_car = [col for col in dataframe.columns if dataframe[col].nunique() > car_th and
+                   dataframe[col].dtype == "O"]
+    cat_cols = cat_cols + num_but_cat
+    cat_cols = [col for col in cat_cols if col not in cat_but_car]
+
+    # num_cols
+    num_cols = [col for col in dataframe.columns if dataframe[col].dtype != "O"]
+    num_cols = [col for col in num_cols if col not in num_but_cat]
+
+    print(f"Observations: {dataframe.shape[0]}")
+    print(f"Variables: {dataframe.shape[1]}")
+    print(f'cat_cols: {len(cat_cols)}')
+    print(f'num_cols: {len(num_cols)}')
+    print(f'cat_but_car: {len(cat_but_car)}')
+    print(f'num_but_cat: {len(num_but_cat)}')
+    return cat_cols, num_cols, cat_but_car
+
+def grab_outliers(dataframe, col_name, index=False):
+    low, up = outlier_thresholds(dataframe, col_name)
+
+    if dataframe[((dataframe[col_name] < low) | (dataframe[col_name] > up))].shape[0] > 10:
+        print(dataframe[((dataframe[col_name] < low) | (dataframe[col_name] > up))].head())
+    else:
+        print(dataframe[((dataframe[col_name] < low) | (dataframe[col_name] > up))])
+
+    if index:
+        outlier_index = dataframe[((dataframe[col_name] < low) | (dataframe[col_name] > up))].index
+        return outlier_index
+
+def remove_outlier(dataframe, col_name):
+    low_limit, up_limit = outlier_thresholds(dataframe, col_name)
+    df_without_outliers = dataframe[~((dataframe[col_name] < low_limit) | (dataframe[col_name] > up_limit))]
+    return df_without_outliers
+
+def replace_with_thresholds(dataframe, variable):
+    low_limit, up_limit = outlier_thresholds(dataframe, variable)
+    dataframe.loc[(dataframe[variable] < low_limit), variable] = low_limit
+    dataframe.loc[(dataframe[variable] > up_limit), variable] = up_limit
+
+def missing_values_table(dataframe, na_name=False):
+    na_columns = [col for col in dataframe.columns if dataframe[col].isnull().sum() > 0]
+
+    n_miss = dataframe[na_columns].isnull().sum().sort_values(ascending=False)
+    ratio = (dataframe[na_columns].isnull().sum() / dataframe.shape[0] * 100).sort_values(ascending=False)
+    missing_df = pd.concat([n_miss, np.round(ratio, 2)], axis=1, keys=['n_miss', 'ratio'])
+    print(missing_df, end="\n")
+
+    if na_name:
+        return na_columns
+
+def missing_vs_target(dataframe, target, na_columns):
+    temp_df = dataframe.copy()
+
+    for col in na_columns:
+        temp_df[col + '_NA_FLAG'] = np.where(temp_df[col].isnull(), 1, 0)
+
+    na_flags = temp_df.loc[:, temp_df.columns.str.contains("_NA_")].columns
+
+    for col in na_flags:
+        print(pd.DataFrame({"TARGET_MEAN": temp_df.groupby(col)[target].mean(),
+                            "Count": temp_df.groupby(col)[target].count()}), end="\n\n\n")
+
+def label_encoder(dataframe, binary_col):
+    labelencoder = LabelEncoder()
+    dataframe[binary_col] = labelencoder.fit_transform(dataframe[binary_col])
+    return dataframe
+
+def one_hot_encoder(dataframe, categorical_cols, drop_first=True):
+    dataframe = pd.get_dummies(dataframe, columns=categorical_cols, drop_first=drop_first)
+    return dataframe
+
+def cat_summary(dataframe, col_name, plot=False):
+    print(pd.DataFrame({col_name: dataframe[col_name].value_counts(),
+                        "Ratio": 100 * dataframe[col_name].value_counts() / len(dataframe)}))
+    print("##########################################")
+    if plot:
+        sns.countplot(x=dataframe[col_name], data=dataframe)
+        plt.show(block=True)
+
+def rare_analyser(dataframe, target, cat_cols):
+    for col in cat_cols:
+        print(col, ":", len(dataframe[col].value_counts()))
+        print(pd.DataFrame({"COUNT": dataframe[col].value_counts(),
+                            "RATIO": dataframe[col].value_counts() / len(dataframe),
+                            "TARGET_MEAN": dataframe.groupby(col)[target].mean()}), end="\n\n\n")
+
+def rare_encoder(dataframe, rare_perc):
+    temp_df = dataframe.copy()
+
+    rare_columns = [col for col in temp_df.columns if temp_df[col].dtypes == 'O'
+                    and (temp_df[col].value_counts() / len(temp_df) < rare_perc).any(axis=None)]
+
+    for var in rare_columns:
+        tmp = temp_df[var].value_counts() / len(temp_df)
+        rare_labels = tmp[tmp < rare_perc].index
+        temp_df[var] = np.where(temp_df[var].isin(rare_labels), 'Rare', temp_df[var])
+
+    return temp_df
+
+def num_summary(dataframe, numerical_col, plot=False):
+    quantiles = [0.05, 0.10, 0.20, 0.30, 0.40, 0.50, 0.60, 0.70, 0.80, 0.90, 0.95, 0.99]
+    print(dataframe[numerical_col].describe(quantiles).T)
+
+    if plot:
+        dataframe[numerical_col].hist(bins=20)
+        plt.xlabel(numerical_col)
+        plt.title(numerical_col)
+        plt.show(block=True)
+
+def plot_feature_importance(importance,names,model_type):
+    # Create arrays from feature importance and feature names
+    feature_importance = np.array(importance)
+    feature_names = np.array(names)
+
+    # Create a DataFrame using a Dictionary
+    data = {'feature_names': feature_names, 'feature_importance': feature_importance}
+    fi_df = pd.DataFrame(data)
+
+    # Sort the DataFrame in order decreasing feature importance
+    fi_df.sort_values(by=['feature_importance'], ascending=False, inplace=True)
+
+    # Define size of bar plot
+    plt.figure(figsize=(25, 10))
+    # Plot Searborn bar chart
+    sns.barplot(x=fi_df['feature_importance'], y=fi_df['feature_names'])
+    # Add chart labels
+    plt.title(model_type + ' FEATURE IMPORTANCE')
+    plt.xlabel('FEATURE IMPORTANCE')
+    plt.ylabel('FEATURE NAMES')
+    plt.show()
+
+def high_correlated_cols(dataframe, plot=False, corr_th=0.90):
+    corr = dataframe.corr()
+    cor_matrix = corr.abs()
+    upper_triangle_matrix = cor_matrix.where(np.triu(np.ones(cor_matrix.shape), k=1).astype(np.bool))
+    drop_list = [col for col in upper_triangle_matrix.columns if any(upper_triangle_matrix[col] > corr_th)]
+    if plot:
+        import seaborn as sns
+        import matplotlib.pyplot as plt
+        sns.set(rc={'figure.figsize': (15, 15)})
+        sns.heatmap(corr, cmap="RdBu")
+        plt.show()
+    return drop_list
+
+def target_correlation_matrix(dataframe, corr_th=0.5, target="Salary"):
+    """
+    Bağımlı değişken ile verilen threshold değerinin üzerindeki korelasyona sahip değişkenleri getirir.
+    :param dataframe:
+    :param corr_th: eşik değeri
+    :param target:  bağımlı değişken ismi
+    :return:
+    """
+    corr = dataframe.corr()
+    corr_th = corr_th
+    try:
+        filter = np.abs(corr[target]) > corr_th
+        corr_features = corr.columns[filter].tolist()
+        sns.clustermap(dataframe[corr_features].corr(), annot=True, fmt=".2f")
+        plt.show()
+        return corr_features
+    except:
+        print("Yüksek threshold değeri, corr_th değerinizi düşürün!")
+
+
+def all_models(X, y, test_size=0.2, random_state=12345, classification=True):
+    from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, \
+        roc_auc_score, confusion_matrix, classification_report, plot_roc_curve, mean_squared_error
+
+    # Tum Base Modeller (Classification)
+    from sklearn.linear_model import LogisticRegression
+    from sklearn.neighbors import KNeighborsClassifier
+    from sklearn.tree import DecisionTreeClassifier
+    from sklearn.ensemble import RandomForestClassifier
+    from sklearn.ensemble import GradientBoostingClassifier
+    from catboost import CatBoostClassifier
+    from lightgbm import LGBMClassifier
+    from sklearn.svm import SVC
+
+    # Tum Base Modeller (Regression)
+    from catboost import CatBoostRegressor
+    from lightgbm import LGBMRegressor
+    from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+    from sklearn.linear_model import LinearRegression, Ridge, Lasso, ElasticNet
+    from sklearn.neighbors import KNeighborsRegressor
+    from sklearn.svm import SVR
+    from sklearn.tree import DecisionTreeRegressor
+    from xgboost import XGBRegressor
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=random_state)
+    all_models = []
+
+    if classification:
+        models = [('LR', LogisticRegression(random_state=random_state)),
+                  ('KNN', KNeighborsClassifier()),
+                  ('CART', DecisionTreeClassifier(random_state=random_state)),
+                  ('RF', RandomForestClassifier(random_state=random_state)),
+                  ('SVM', SVC(gamma='auto', random_state=random_state)),
+                  ('XGB', GradientBoostingClassifier(random_state=random_state)),
+                  ("LightGBM", LGBMClassifier(random_state=random_state)),
+                  ("CatBoost", CatBoostClassifier(verbose=False, random_state=random_state))]
+
+        for name, model in models:
+            model.fit(X_train, y_train)
+            y_pred_train = model.predict(X_train)
+            y_pred_test = model.predict(X_test)
+            acc_train = accuracy_score(y_train, y_pred_train)
+            acc_test = accuracy_score(y_test, y_pred_test)
+            values = dict(name=name, acc_train=acc_train, acc_test=acc_test)
+            all_models.append(values)
+
+        sort_method = False
+    else:
+        models = [('LR', LinearRegression()),
+                  ("Ridge", Ridge()),
+                  ("Lasso", Lasso()),
+                  ("ElasticNet", ElasticNet()),
+                  ('KNN', KNeighborsRegressor()),
+                  ('CART', DecisionTreeRegressor()),
+                  ('RF', RandomForestRegressor()),
+                  ('SVR', SVR()),
+                  ('GBM', GradientBoostingRegressor()),
+                  ("XGBoost", XGBRegressor()),
+                  ("LightGBM", LGBMRegressor()),
+                  ("CatBoost", CatBoostRegressor(verbose=False))]
+
+        for name, model in models:
+            model.fit(X_train, y_train)
+            y_pred_test = model.predict(X_test)
+            y_pred_train = model.predict(X_train)
+            rmse_test = np.sqrt(mean_squared_error(y_test, y_pred_test))
+            rmse_train = np.sqrt(mean_squared_error(y_train, y_pred_train))
+            values = dict(name=name, RMSE_TRAIN=rmse_train, RMSE_TEST=rmse_test)
+            all_models.append(values)
+
+        sort_method = True
+    all_models_df = pd.DataFrame(all_models)
+    all_models_df = all_models_df.sort_values(all_models_df.columns[2], ascending=sort_method)
+    print(all_models_df)
+    return all_models_df
+
+all_models = all_models(X, y, test_size=0.2, random_state=46, classification=False)
+
+
+#####################################################################################
+# GRADIENT DESCENT FROM SCRATCH
+#####################################################################################
+
+# Cost function MSE
+def cost_function(Y, b, w, X):
+    m = len(Y)
+    sse = 0
+
+    for i in range(0, m):
+        y_hat = b + w * X[i]
+        y = Y[i]
+        sse += (y_hat - y) ** 2
+
+    mse = sse / m
+    return mse
+
+
+# update_weights
+def update_weights(Y, b, w, X, learning_rate):
+    m = len(Y)
+    b_deriv_sum = 0
+    w_deriv_sum = 0
+    for i in range(0, m):
+        y_hat = b + w * X[i]
+        y = Y[i]
+        b_deriv_sum += (y_hat - y)
+        w_deriv_sum += (y_hat - y) * X[i]
+    new_b = b - (learning_rate * 1 / m * b_deriv_sum)
+    new_w = w - (learning_rate * 1 / m * w_deriv_sum)
+    return new_b, new_w
+
+
+# train fonksiyonu
+def train(Y, initial_b, initial_w, X, learning_rate, num_iters):
+
+    print("Starting gradient descent at b = {0}, w = {1}, mse = {2}".format(initial_b, initial_w,
+                                                                   cost_function(Y, initial_b, initial_w, X)))
+
+    b = initial_b
+    w = initial_w
+    cost_history = []
+
+    for i in range(num_iters):
+        b, w = update_weights(Y, b, w, X, learning_rate)
+        mse = cost_function(Y, b, w, X)
+        cost_history.append(mse)
+
+
+        if i % 100 == 0:
+            print("iter={:d}    b={:.2f}    w={:.4f}    mse={:.4}".format(i, b, w, mse))
+
+
+    print("After {0} iterations b = {1}, w = {2}, mse = {3}".format(num_iters, b, w, cost_function(Y, b, w, X)))
+    return cost_history, b, w
+
+def correlation_matrix(df, cols):
+    fig = plt.gcf()
+    fig.set_size_inches(10, 8)
+    plt.xticks(fontsize=10)
+    plt.yticks(fontsize=10)
+    fig = sns.heatmap(df[cols].corr(), annot=True, linewidths=0.5, annot_kws={'size': 12}, linecolor='w', cmap='RdBu')
+    plt.show(block=True)
+
+def plot_confusion_matrix(y, y_pred):
+    acc = round(accuracy_score(y, y_pred), 2)
+    cm = confusion_matrix(y, y_pred)
+    sns.heatmap(cm, annot=True, fmt=".0f")
+    plt.xlabel('y_pred')
+    plt.ylabel('y')
+    plt.title('Accuracy Score: {0}'.format(acc), size=10)
+    plt.show()
