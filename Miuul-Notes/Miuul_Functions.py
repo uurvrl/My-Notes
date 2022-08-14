38aef399c3853290419f42937544cd9e1b68fd19
Ugur Vural <107815698+uurvrl@users.noreply.github.com>
2022-08-14 18:53:02 +0300

    Update Miuul_Functions.py

--- Miuul-Notes/Miuul_Functions.py
+++ Miuul-Notes/Miuul_Functions.py
@@ -445,6 +445,32 @@ def plot_confusion_matrix(y, y_pred):
     plt.title('Accuracy Score: {0}'.format(acc), size=10)
     plt.show()
 
+def create_pca_df(X, y):
+    X = StandardScaler().fit_transform(X)
+    pca = PCA(n_components=2)
+    pca_fit = pca.fit_transform(X)
+    pca_df = pd.DataFrame(data=pca_fit, columns=['PC1', 'PC2'])
+    final_df = pd.concat([pca_df, pd.DataFrame(y)], axis=1)
+    return final_df
+
+pca_df = create_pca_df(X, y)
+
+def plot_pca(dataframe, target):
+    fig = plt.figure(figsize=(7, 5))
+    ax = fig.add_subplot(1, 1, 1)
+    ax.set_xlabel('PC1', fontsize=15)
+    ax.set_ylabel('PC2', fontsize=15)
+    ax.set_title(f'{target.capitalize()} ', fontsize=20)
+
+    targets = list(dataframe[target].unique())
+    colors = random.sample(['r', 'b', "g", "y"], len(targets))
+
+    for t, color in zip(targets, colors):
+        indices = dataframe[target] == t
+        ax.scatter(dataframe.loc[indices, 'PC1'], dataframe.loc[indices, 'PC2'], c=color, s=50)
+    ax.legend(targets)
+    ax.grid()
+    plt.show()    
 
 def val_curve_params(model, X, y, param_name, param_range, scoring="roc_auc", cv=10):
     train_score, test_score = validation_curve(
